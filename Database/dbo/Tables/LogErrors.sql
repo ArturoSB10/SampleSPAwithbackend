f24e16e8fa239b99b84164aa2710731dad937bdf
ArturoEsteban443 <116023157+ArturoEsteban443@users.noreply.github.com>
2023-09-13 00:08:56 -0600

    Save database project

---
+++ Database/dbo/Tables/LogErrors.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LogErrors] (
+    [id]               INT            IDENTITY (1, 1) NOT NULL,
+    [error]            NVARCHAR (MAX) NULL,
+    [registrationDate] AS             (getdate()),
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ Database/dbo/Tables/Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [id]               INT           IDENTITY (1, 1) NOT NULL,
+    [userName]         NVARCHAR (50) NOT NULL,
+    [password]         NVARCHAR (50) NOT NULL,
+    [isAdmin]          BIT           NOT NULL,
+    [registrationDate] AS            (getdate()),
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    UNIQUE NONCLUSTERED ([userName] ASC)
+);
+
